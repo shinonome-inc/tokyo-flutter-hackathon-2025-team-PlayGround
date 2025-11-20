5b64a8669ec290fa268b51885d06726cb71daa14
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-20 23:34:51 +0900

    Terraform用IAMユーザーにS3とDynamoDBへのアクセス権限を追加

--- infra/terraform/terraform-setup/iam.tf
+++ infra/terraform/terraform-setup/iam.tf
@@ -11,3 +11,36 @@ resource "aws_iam_user_policy_attachment" "power_user" {
   user       = aws_iam_user.terraform_executor.name
   policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
+
+resource "aws_iam_user_policy" "terraform_backend_access" {
+  name = "TerraformBackendAccess"
+  user = aws_iam_user.terraform_executor.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::genkaimeshi-recipe-terraform-state",
+          "arn:aws:s3:::genkaimeshi-recipe-terraform-state/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:ap-northeast-1:851725222522:table/terraform-lock"
+      }
+    ]
+  })
+}
