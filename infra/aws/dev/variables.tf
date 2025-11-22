8392f634596ca2c3eff1e653fcdc96437a918236
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-23 01:24:09 +0900

    Cognito OAuth用の変数定義を追加
    Google/LINE認証プロバイダーの設定変数を追加

--- infra/aws/dev/variables.tf
+++ infra/aws/dev/variables.tf
@@ -9,3 +9,39 @@ variable "aws_region" {
   type        = string
   default     = "ap-northeast-1"
 }
+
+variable "google_client_id" {
+  description = "Google OAuth クライアントID"
+  type        = string
+  sensitive   = true
+}
+
+variable "google_client_secret" {
+  description = "Google OAuth クライアントシークレット"
+  type        = string
+  sensitive   = true
+}
+
+variable "line_channel_id" {
+  description = "LINE Login チャンネルID"
+  type        = string
+  sensitive   = true
+}
+
+variable "line_channel_secret" {
+  description = "LINE Login チャンネルシークレット"
+  type        = string
+  sensitive   = true
+}
+
+variable "cognito_callback_urls" {
+  description = "Cognito認証後のコールバックURL"
+  type        = list(string)
+  default     = ["myapp://callback"]
+}
+
+variable "cognito_logout_urls" {
+  description = "Cognitoログアウト後のリダイレクトURL"
+  type        = list(string)
+  default     = ["myapp://logout"]
+}
