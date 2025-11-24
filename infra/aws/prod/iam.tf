543bd836c9dbad1bd1fb385b296aaef20bfacb15
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-24 16:59:08 +0900

    Lambda関数用のIAMロールを作成

--- infra/aws/prod/iam.tf
+++ infra/aws/prod/iam.tf
@@ -1,3 +1,21 @@
-data "aws_iam_role" "lambda_basic_execution" {
-  name = "AWSLambdaBasicExecutionRole"
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "${var.project_name}-lambda-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_execution_role.name
 }
