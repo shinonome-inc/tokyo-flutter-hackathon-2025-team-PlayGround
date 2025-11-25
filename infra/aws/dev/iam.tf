80d1fcd7fb7cbe6d68a5dcb974e3702891416aa5
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-26 00:40:42 +0900

    LambdaがS3バケットに画像をアップロードできるようIAM権限を追加

--- infra/aws/dev/iam.tf
+++ infra/aws/dev/iam.tf
@@ -44,3 +44,22 @@ resource "aws_iam_role_policy" "lambda_secrets_manager_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy" "lambda_s3_policy" {
+  name = "${var.project_name}-lambda-s3-policy"
+  role = aws_iam_role.lambda_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "${aws_s3_bucket.recipe_images.arn}/*"
+      }
+    ]
+  })
+}
