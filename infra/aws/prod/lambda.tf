0154d90945a8bf65ef7d4063a23be5b53a8bb0f2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 01:01:02 +0900

    aws/prod内にある.tfのフォーマットを修正

--- infra/aws/prod/lambda.tf
+++ infra/aws/prod/lambda.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "recipe_ai_generator" {
 
   environment {
     variables = {
-      GEMINI_API_KEY_ARN         = aws_secretsmanager_secret.gemini_api_key.arn
+      GEMINI_API_KEY_ARN          = aws_secretsmanager_secret.gemini_api_key.arn
       GCP_SERVICE_ACCOUNT_KEY_ARN = aws_secretsmanager_secret.gcp_service_account_key.arn
       S3_BUCKET_NAME              = aws_s3_bucket.recipe_images.bucket
       ENVIRONMENT                 = "prod"
