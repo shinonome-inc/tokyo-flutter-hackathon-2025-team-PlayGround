a16d847fb6589fc7b40f3aa9d7d55bf3c387e63f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-28 21:24:09 +0900

    Presigned URL生成Lambda関数をTerraformに追加

--- infra/aws/prod/lambda.tf
+++ infra/aws/prod/lambda.tf
@@ -32,3 +32,36 @@ resource "aws_lambda_permission" "api_gateway_lambda" {
 
   source_arn = "${aws_api_gateway_rest_api.main_api.execution_arn}/*/*"
 }
+
+# Presigned URL生成Lambda関数
+resource "aws_lambda_function" "presigned_url_generator" {
+  function_name = "${var.project_name}-presigned-url-generator"
+  role          = aws_iam_role.lambda_execution_role.arn
+
+  runtime = "nodejs20.x"
+  handler = "index.handler"
+
+  filename         = "../../../backend/lambda/dist/presigned_url.zip"
+  source_code_hash = filebase64sha256("../../../backend/lambda/dist/presigned_url.zip")
+
+  environment {
+    variables = {
+      S3_BUCKET_NAME = aws_s3_bucket.recipe_images.bucket
+      ENVIRONMENT    = "prod"
+    }
+  }
+
+  timeout     = 10
+  memory_size = 128
+
+  depends_on = [aws_cloudwatch_log_group.presigned_url_lambda_log_group]
+}
+
+resource "aws_lambda_permission" "api_gateway_presigned_url_lambda" {
+  statement_id  = "AllowAPIGatewayInvokePresignedUrl"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.presigned_url_generator.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.main_api.execution_arn}/*/*"
+}
