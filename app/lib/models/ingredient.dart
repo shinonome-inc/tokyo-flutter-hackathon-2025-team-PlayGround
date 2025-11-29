d87fd06dae5074e954437ff1602680053ecc09b8
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 18:47:25 +0900

    レシピの材料情報を管理するためIngredientモデルを追加

---
+++ app/lib/models/ingredient.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'ingredient.freezed.dart';
+part 'ingredient.g.dart';
+
+@freezed
+abstract class Ingredient with _$Ingredient {
+  const factory Ingredient({
+    String? id,
+    String? name,
+    String? amount,
+  }) = _Ingredient;
+
+  factory Ingredient.fromJson(Map<String, Object?> json) =>
+      _$IngredientFromJson(json);
+}
