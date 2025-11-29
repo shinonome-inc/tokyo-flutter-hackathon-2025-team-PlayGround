04a1b7c8ea1da03630f9e741df2c07727e18bd34
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 11:10:25 +0900

    Flutter Web配信用のCloudFront設定を追加

---
+++ infra/aws/dev/cloudfront.tf
@@ -0,0 +1,48 @@
+resource "aws_cloudfront_distribution" "flutter_web" {
+  enabled         = true
+  is_ipv6_enabled = true
+  comment         = "Flutter Web Distribution - Dev"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.flutter_web.website_endpoint
+    origin_id   = "S3-${var.flutter_web_bucket_name}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-${var.flutter_web_bucket_name}"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 0
+    max_ttl     = 0
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
