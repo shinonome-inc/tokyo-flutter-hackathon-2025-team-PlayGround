60e80f41f89f37c26d9020a4b4d4db995491d87d
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 18:07:06 +0900

    RecipeListScreenにレシピ詳細とユーザー詳細への画面遷移を追加

--- app/lib/screens/recipe_list/recipe_list_screen.dart
+++ app/lib/screens/recipe_list/recipe_list_screen.dart
@@ -1,5 +1,7 @@
+import 'package:app/enums/app_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class RecipeListScreen extends ConsumerStatefulWidget {
   const RecipeListScreen({super.key});
@@ -9,8 +11,30 @@ class RecipeListScreen extends ConsumerStatefulWidget {
 }
 
 class _RecipeListScreenState extends ConsumerState<RecipeListScreen> {
+  void _onTapRecipeItem() {
+    context.go(AppPage.recipeDetail.path);
+  }
+
+  void _onTapUser() {
+    context.go(AppPage.userDetail.path);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Recipe List')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Recipe List')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _onTapRecipeItem,
+              child: const Text('レシピ詳細へ'),
+            ),
+            ElevatedButton(onPressed: _onTapUser, child: const Text('ユーザー詳細へ')),
+          ],
+        ),
+      ),
+    );
   }
 }
