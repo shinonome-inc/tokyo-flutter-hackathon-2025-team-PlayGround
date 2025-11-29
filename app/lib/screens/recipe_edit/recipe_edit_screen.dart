e390966006b6097d16602287f79066e81a814960
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 18:07:00 +0900

    RecipeEditScreenに編集完了後の画面遷移を追加

--- app/lib/screens/recipe_edit/recipe_edit_screen.dart
+++ app/lib/screens/recipe_edit/recipe_edit_screen.dart
@@ -1,5 +1,7 @@
+import 'package:app/enums/app_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class RecipeEditScreen extends ConsumerStatefulWidget {
   const RecipeEditScreen({super.key});
@@ -9,8 +11,22 @@ class RecipeEditScreen extends ConsumerStatefulWidget {
 }
 
 class _RecipeEditScreenState extends ConsumerState<RecipeEditScreen> {
+  void _onTapConfirm() {
+    context.go(AppPage.recipeDetail.path);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Recipe Edit')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Recipe Edit')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: _onTapConfirm, child: const Text('完了')),
+          ],
+        ),
+      ),
+    );
   }
 }
