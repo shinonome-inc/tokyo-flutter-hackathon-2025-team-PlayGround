766299e79778e704f10f7865365a1cbc79c41f92
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 14:03:51 +0900

    Webでデプロイしたページに何も表示されない不具合を修正するため、環境変数の管理方式を変更

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,26 +1,2 @@
-import 'package:app/config/amplify_initializer.dart';
-import 'package:app/screens/sample_screen/sample_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await configureAmplify();
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Genkaimeshi Recipe',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SampleScreen(),
-    );
-  }
-}
+// デフォルトはdev環境で起動
+export 'main_dev.dart';
