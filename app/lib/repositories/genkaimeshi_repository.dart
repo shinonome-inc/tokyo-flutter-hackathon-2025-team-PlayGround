5cac42ac3ed692ca4f320eefe3b5650e4c2e2088
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 22:34:38 +0900

    GenkaimeshiRepositoryにbaseUrlを追加

--- app/lib/repositories/genkaimeshi_repository.dart
+++ app/lib/repositories/genkaimeshi_repository.dart
@@ -1,3 +1,6 @@
+import 'package:app/enums/app_env.dart';
+
+/// 限界飯APIのリポジトリクラス。
 class GenkaimeshiRepository {
   factory GenkaimeshiRepository() {
     return _instance;
@@ -7,4 +10,18 @@ class GenkaimeshiRepository {
 
   static final GenkaimeshiRepository _instance =
       GenkaimeshiRepository._internal();
+
+  AppEnv _currentEnvironment = AppEnv.dev;
+  AppEnv get currentEnvironment => _currentEnvironment;
+
+  static const Map<AppEnv, String> _baseUrls = {
+    AppEnv.dev:
+        'https://a71hr6sq8c.execute-api.ap-northeast-1.amazonaws.com/dev',
+    AppEnv.prod: 'https://a71hr6sq8c.execute-api.ap-northeast-1.amazonaws.com/',
+  };
+  String get baseUrl => _baseUrls[_currentEnvironment]!;
+
+  void setEnvironment(AppEnv environment) {
+    _currentEnvironment = environment;
+  }
 }
