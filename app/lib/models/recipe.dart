bc0a104390620b37823ca8e0dd469bd6df3b5908
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-30 00:26:36 +0900

    Recipeクラスの引数をrequiredに変更

--- app/lib/models/recipe.dart
+++ app/lib/models/recipe.dart
@@ -10,19 +10,19 @@ part 'recipe.g.dart';
 @freezed
 abstract class Recipe with _$Recipe {
   const factory Recipe({
-    String? id,
-    String? title,
-    String? overview,
-    String? imageUrl,
-    bool? isAiGenerated,
-    DateTime? createdAt,
-    User? user,
-    String? notes,
-    List<Ingredient>? ingredients,
-    List<Step>? steps,
-    List<Comment>? comments,
-    bool? isLikedByMe,
-    int? likeCount,
+    required String id,
+    required String title,
+    required String overview,
+    required String imageUrl,
+    required bool isAiGenerated,
+    required DateTime createdAt,
+    required User user,
+    required String notes,
+    required List<Ingredient> ingredients,
+    required List<Step> steps,
+    required List<Comment> comments,
+    required bool isLikedByMe,
+    required int likeCount,
   }) = _Recipe;
 
   factory Recipe.fromJson(Map<String, Object?> json) => _$RecipeFromJson(json);
