ac43def608a46ff538a09ecc41ceded885ba5280
Yoshida-koshi <koshi2278@outlook.jp>
2025-11-29 11:56:28 +0900

    POST /recipesのAPI実装

--- infra/aws/prod/api_gateway.tf
+++ infra/aws/prod/api_gateway.tf
@@ -56,6 +56,29 @@ resource "aws_api_gateway_integration" "presigned_url_integration" {
   uri                     = aws_lambda_function.presigned_url_generator.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "post_recipe_resource" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  parent_id = aws_api_gateway_resource.v1.id
+  path_part = "recipes"
+}
+
+resource "aws_api_gateway_method" "post_recipe_method" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.post_recipe_resource.id
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "post_recipe_integration" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.post_recipe_resource.id
+  http_method = aws_api_gateway_method.post_recipe_method.http_method
+
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.post_recipe.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "main_deployment" {
   depends_on = [
     aws_api_gateway_integration.recipe_ai_integration,
--- infra/aws/prod/cloudwatch.tf
+++ infra/aws/prod/cloudwatch.tf
@@ -7,3 +7,8 @@ resource "aws_cloudwatch_log_group" "presigned_url_lambda_log_group" {
   name              = "/aws/lambda/${var.project_name}-presigned-url-generator"
   retention_in_days = 14
 }
+
+resource "aws_cloudwatch_log_group" "post_recipe_log_group" {
+  name = "/aws/lambda/${var.project_name}-post-recipe"
+  retention_in_days = 14
+}
