245e221bf83dc5afe2f0691778cf4b42bf0d87e2
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 18:07:26 +0900

    UserCreateScreenにユーザー登録とスキップ時の画面遷移を追加

--- app/lib/screens/user_create/user_create_screen.dart
+++ app/lib/screens/user_create/user_create_screen.dart
@@ -1,5 +1,7 @@
+import 'package:app/enums/app_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class UserCreateScreen extends ConsumerStatefulWidget {
   const UserCreateScreen({super.key});
@@ -9,8 +11,35 @@ class UserCreateScreen extends ConsumerStatefulWidget {
 }
 
 class _UserCreateScreenState extends ConsumerState<UserCreateScreen> {
+  void _onTapCreateUser() {
+    // ユーザー情報作成後、ボトムナビゲーションバーの画面へ遷移
+    context.go(AppPage.recipeList.path); // TODO: BottomNavigationScreenに変更予定
+  }
+
+  void _onTapSkip() {
+    // スキップした場合もボトムナビゲーションバーの画面へ遷移
+    context.go(AppPage.recipeList.path); // TODO: BottomNavigationScreenに変更予定
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('User Create')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('ユーザー情報登録')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _onTapCreateUser,
+              child: const Text('ユーザー情報を登録'),
+            ),
+            TextButton(
+              onPressed: _onTapSkip,
+              child: const Text('スキップ'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
