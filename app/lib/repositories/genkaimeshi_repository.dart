52c01f95c85c970e3b2b482186c61d4be0ce4b87
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 22:54:59 +0900

    Dioを追加

--- app/lib/repositories/genkaimeshi_repository.dart
+++ app/lib/repositories/genkaimeshi_repository.dart
@@ -1,27 +1,25 @@
 import 'package:app/enums/app_env.dart';
+import 'package:app/models/recipe_detail.dart';
+import 'package:dio/dio.dart';
 
 /// 限界飯APIのリポジトリクラス。
 class GenkaimeshiRepository {
-  factory GenkaimeshiRepository() {
-    return _instance;
+  GenkaimeshiRepository({
+    Dio? dio,
+    AppEnv environment = AppEnv.dev,
+  }) : _dio = dio ?? Dio() {
+    _dio.options.baseUrl = _getBaseUrl(environment);
   }
 
-  GenkaimeshiRepository._internal();
-
-  static final GenkaimeshiRepository _instance =
-      GenkaimeshiRepository._internal();
-
-  AppEnv _currentEnvironment = AppEnv.dev;
-  AppEnv get currentEnvironment => _currentEnvironment;
+  final Dio _dio;
 
   static const Map<AppEnv, String> _baseUrls = {
     AppEnv.dev:
         'https://a71hr6sq8c.execute-api.ap-northeast-1.amazonaws.com/dev',
     AppEnv.prod: 'https://a71hr6sq8c.execute-api.ap-northeast-1.amazonaws.com/',
   };
-  String get baseUrl => _baseUrls[_currentEnvironment]!;
 
-  void setEnvironment(AppEnv environment) {
-    _currentEnvironment = environment;
+  static String _getBaseUrl(AppEnv environment) {
+    return _baseUrls[environment]!;
   }
 }
