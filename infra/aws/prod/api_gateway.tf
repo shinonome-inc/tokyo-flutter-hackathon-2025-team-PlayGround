184bb51520e1a114c648c37480ea828ecf76260c
luck-tech <luck@imai.am>
2025-11-29 14:45:05 +0900

    ルーティング追加

--- infra/aws/prod/api_gateway.tf
+++ infra/aws/prod/api_gateway.tf
@@ -56,10 +56,79 @@ resource "aws_api_gateway_integration" "presigned_url_integration" {
   uri                     = aws_lambda_function.presigned_url_generator.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "recipes_resource" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  parent_id   = aws_api_gateway_resource.v1.id
+  path_part   = "recipes"
+}
+
+resource "aws_api_gateway_method" "recipes_get_method" {
+  rest_api_id   = aws_api_gateway_rest_api.main_api.id
+  resource_id   = aws_api_gateway_resource.recipes_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "recipes_get_integration" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.recipes_resource.id
+  http_method = aws_api_gateway_method.recipes_get_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_recipes.invoke_arn
+}
+
+resource "aws_api_gateway_method" "recipes_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.main_api.id
+  resource_id   = aws_api_gateway_resource.recipes_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "recipes_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.recipes_resource.id
+  http_method = aws_api_gateway_method.recipes_options_method.http_method
+
+  type = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "recipes_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.recipes_resource.id
+  http_method = aws_api_gateway_method.recipes_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "recipes_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.main_api.id
+  resource_id = aws_api_gateway_resource.recipes_resource.id
+  http_method = aws_api_gateway_method.recipes_options_method.http_method
+  status_code = aws_api_gateway_method_response.recipes_options_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "main_deployment" {
   depends_on = [
     aws_api_gateway_integration.recipe_ai_integration,
-    aws_api_gateway_integration.presigned_url_integration
+    aws_api_gateway_integration.presigned_url_integration,
+    aws_api_gateway_integration.recipes_get_integration,
+    aws_api_gateway_integration.recipes_options_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.main_api.id
