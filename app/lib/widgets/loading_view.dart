be1fcbf2ced9d2a24bd1fb48ffdb3114a67fcfcf
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-30 01:04:21 +0900

    LoadingViewを追加

---
+++ app/lib/widgets/loading_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// ローディング表示用のウィジェット
+///
+class LoadingView extends StatelessWidget {
+  const LoadingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
