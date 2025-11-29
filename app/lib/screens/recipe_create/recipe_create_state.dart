609c008e1bcdb62148cb67cf763ee44d37da1395
kota78 <kota1725@gmail.com>
2025-11-30 06:56:52 +0900

    レシピ作成画面の状態を表すクラスを追加

---
+++ app/lib/screens/recipe_create/recipe_create_state.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'recipe_create_state.freezed.dart';
+
+/// レシピ作成画面の状態を表すクラス
+@freezed
+abstract class RecipeCreateState with _$RecipeCreateState {
+  const factory RecipeCreateState({
+    @Default(false) bool isLoading,
+    @Default(false) bool hasNetworkError,
+    @Default(false) bool isSuccess,
+    @Default('') String errorMessage,
+  }) = _RecipeCreateState;
+}
