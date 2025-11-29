52a9c980c407ea92629b0b46d162f17b01c68205
luck-tech <luck@imai.am>
2025-11-29 14:45:50 +0900

    dynamodbの追加

---
+++ infra/aws/dev/dynamodb.tf
@@ -0,0 +1,86 @@
+resource "aws_dynamodb_table" "main_table" {
+  name         = "${var.project_name}-dev-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
+
+  attribute {
+    name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI1PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI1SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI2PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI2SK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI3PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "GSI3SK"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "GSI1"
+    hash_key        = "GSI1PK"
+    range_key       = "GSI1SK"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "GSI2"
+    hash_key        = "GSI2PK"
+    range_key       = "GSI2SK"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "GSI3"
+    hash_key        = "GSI3PK"
+    range_key       = "GSI3SK"
+    projection_type = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = var.project_name
+  }
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.main_table.name
+  description = "DynamoDBテーブル名"
+}
+
+output "dynamodb_table_arn" {
+  value       = aws_dynamodb_table.main_table.arn
+  description = "DynamoDBテーブルARN"
+}
