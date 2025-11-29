46bf005e65efa10f65d970db32e3aab82b8aed8f
luck-tech <luck@imai.am>
2025-11-29 14:45:16 +0900

    cloudwatch追加

--- infra/aws/dev/cloudwatch.tf
+++ infra/aws/dev/cloudwatch.tf
@@ -7,3 +7,8 @@ resource "aws_cloudwatch_log_group" "presigned_url_lambda_log_group" {
   name              = "/aws/lambda/${var.project_name}-presigned-url-generator"
   retention_in_days = 14
 }
+
+resource "aws_cloudwatch_log_group" "get_recipes_lambda_log_group" {
+  name              = "/aws/lambda/${var.project_name}-get-recipes"
+  retention_in_days = 14
+}
