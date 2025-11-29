7d26dd7cdc377717777e3a6dbc11be5b39a843d1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-29 11:10:01 +0900

    Flutter Webホスティング用のS3バケット設定を追加

--- infra/aws/prod/s3.tf
+++ infra/aws/prod/s3.tf
@@ -41,3 +41,47 @@ resource "aws_s3_bucket_cors_configuration" "recipe_images" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket" "flutter_web" {
+  bucket = var.flutter_web_bucket_name
+}
+
+resource "aws_s3_bucket_public_access_block" "flutter_web" {
+  bucket = aws_s3_bucket.flutter_web.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "flutter_web" {
+  bucket = aws_s3_bucket.flutter_web.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.flutter_web.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.flutter_web]
+}
+
+resource "aws_s3_bucket_website_configuration" "flutter_web" {
+  bucket = aws_s3_bucket.flutter_web.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
