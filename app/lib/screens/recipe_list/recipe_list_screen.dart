3632a7c1865829872d6561dfeb9bead337d99275
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2025-11-30 01:18:32 +0900

    RecipeListItemを追加

--- app/lib/screens/recipe_list/recipe_list_screen.dart
+++ app/lib/screens/recipe_list/recipe_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:app/enums/app_page.dart';
 import 'package:app/screens/recipe_list/recipe_list_notifier.dart';
+import 'package:app/screens/recipe_list/recipe_list_state.dart';
 import 'package:app/widgets/loading_view.dart';
 import 'package:app/widgets/network_error_view.dart';
 import 'package:flutter/material.dart';
@@ -29,26 +30,23 @@ class _RecipeListScreenState extends ConsumerState<RecipeListScreen> {
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(recipeListProvider);
-    if (state.isLoading) {
-      return const Scaffold(body: LoadingView());
-    } else if (state.hasNetworkError) {
-      return Scaffold(body: NetworkErrorView(onTapReload: _onTapReload));
-    }
     return Scaffold(
       appBar: AppBar(title: const Text('Recipe List')),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (final recipe in state.recipes)
-              ListTile(title: Text(recipe.id)),
-            ElevatedButton(
-              onPressed: _onTapRecipeItem,
-              child: const Text('レシピ詳細へ'),
-            ),
-            ElevatedButton(onPressed: _onTapUser, child: const Text('ユーザー詳細へ')),
-          ],
-        ),
+        child: switch (state) {
+          RecipeListState(:final isLoading) when isLoading =>
+            const LoadingView(),
+          RecipeListState(:final hasNetworkError) when hasNetworkError =>
+            NetworkErrorView(onTapReload: _onTapReload),
+          _ => Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ...state.recipes.map(
+                (recipe) => ListTile(title: Text(recipe.id)),
+              ),
+            ],
+          ),
+        },
       ),
     );
   }
