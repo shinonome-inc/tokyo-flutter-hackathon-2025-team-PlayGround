4333b037fd27447602794ec719d35cc157855e0b
kota78 <kota1725@gmail.com>
2025-11-30 11:20:05 +0900

    S3バケットのCORS設定にPUTおよびPOSTメソッドを追加

--- infra/aws/prod/s3.tf
+++ infra/aws/prod/s3.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_cors_configuration" "recipe_images" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD"]
+    allowed_methods = ["GET", "HEAD", "PUT", "POST"]
     allowed_origins = ["*"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
